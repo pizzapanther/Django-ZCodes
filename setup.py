a58dbe6862a3b8ed21e9c4606a83975e2184accd
Paul Bailey <paul.m.bailey@gmail.com>
2015-05-20 17:03:27 +0000

    setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,16 @@
 from setuptools import setup, find_packages
-# To use a consistent encoding
-from codecs import open
-from os import path
-
-here = path.abspath(path.dirname(__file__))
 
 setup(
-  name='django-zcodes',
-  version='1.0.0',
-
-  description='A sample Python project',
-  long_description="Django data model for zip codes in GeoDjango with preloaded data for the US.",
-  url='https://github.com/pypa/sampleproject',
-  author='Paul Bailey',
-  author_email='paul.m.bailey@gmail.com',
-  
-  license='MIT',
+  name = "django-zcodes",
+  version = '1.0.0',
+  description = "Django data model for zip codes in GeoDjango with preloaded data for the US.",
+  url = "https://github.com/pizzapanther/Django-ZCodes",
+  author = "Paul Bailey",
+  author_email = "paul.m.bailey@gmail.com",
+  license = "MIT",
+  packages = ['zcodes', 'zcodes.migrations'],
+  include_package_data = True,
+  data_files=[('zcodes/fixtures', ['zcodes/fixtures/zcodes_us.json'])],
   
   classifiers=[
     'Development Status :: 5 - Production/Stable',
@@ -23,17 +18,4 @@ setup(
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 2.7',
   ],
-  
-  keywords='django geodjango zip codes',
-
-  packages=find_packages(),
-  
-  install_requires=[],
-
-  extras_require={
-    'dev': [],
-    'test': [],
-  },
-  
-  data_files=[('zcode/fixtures', ['zcode/fixtures/zcodes_us.json'])],
 )
